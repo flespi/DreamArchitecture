9cf8fb0c5123bb1b28c31c1ee110eadf93afc810
Facundo Lespi <facu.lespi@gmail.com>
2026-05-12 20:06:56 -0300

    🧑‍💻 Improve migration generation

--- src/Infrastructure/Data/ApplicationDbContextFactory.cs
+++ src/Infrastructure/Data/ApplicationDbContextFactory.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System.CommandLine;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
 
 namespace CleanArchitecture.Infrastructure.Data;
@@ -7,9 +8,47 @@ public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<Applicati
 {
     public ApplicationDbContext CreateDbContext(string[] args)
     {
+        var options = ParseArgs(args);
+
         var builder = new DbContextOptionsBuilder<ApplicationDbContext>();
-        builder.UseSqlServer();
+
+        switch (options.Provider?.ToLower())
+        {
+            case null:
+            case "sqlserver":
+                builder.UseSqlServer(options.ConnectionString);
+                break;
+            default:
+                throw new ApplicationException("Unknown database provider.");
+        }
 
         return new ApplicationDbContext(builder.Options);
+    }
+
+    public static DbContextFactoryOptions ParseArgs(string[] args)
+    {
+        var connectionOption = new Option<string?>("--connection-string", "-c")
+        {
+            Description = "Connection String"
+        };
+
+        var providerOption = new Option<string?>("--provider", "-p")
+        {
+            Description = "Provider"
+        };
+
+        var root = new RootCommand
+        {
+            connectionOption,
+            providerOption,
+        };
+
+        var result = root.Parse(args);
+
+        return new DbContextFactoryOptions
+        {
+            ConnectionString = result.GetValue(connectionOption),
+            Provider = result.GetValue(providerOption),
+        };
     }
 }
---
+++ src/Infrastructure/Data/DbContextFactoryOptions.cs
@@ -0,0 +1,8 @@
+﻿namespace CleanArchitecture.Infrastructure.Data;
+
+public class DbContextFactoryOptions
+{
+    public string? ConnectionString { get; set; }
+
+    public string? Provider { get; set; }
+}
